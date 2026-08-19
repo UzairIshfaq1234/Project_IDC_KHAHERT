55556dd99e658d3b7526318cf49c2412bf0a98c4
M-UzairIshfaq <uzairi@logicodetech.com>
2026-08-19 09:42:37 +0500

    seeded admins

--- LaravelPortal/database/seeders/DatabaseSeeder.php
+++ LaravelPortal/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\idc_admin;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +12,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        idc_admin::create([
+            'Name'      => 'Admin',
+            'Username'  => 'admin',
+            'Email'     => 'admin@idc.com',
+            'Password'  => 'Admin@123',
+            'Role'      => 1,
+            'Contactno' => '00000000000',
+            'Image'     => '',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        idc_admin::create([
+            'Name'      => 'Lab Technician',
+            'Username'  => 'labtech',
+            'Email'     => 'labtech@idc.com',
+            'Password'  => 'LabTech@123',
+            'Role'      => 2,
+            'Contactno' => '00000000000',
+            'Image'     => '',
+        ]);
+
+        idc_admin::create([
+            'Name'      => 'Pathologist',
+            'Username'  => 'pathologist',
+            'Email'     => 'pathologist@idc.com',
+            'Password'  => 'Path@123',
+            'Role'      => 3,
+            'Contactno' => '00000000000',
+            'Image'     => '',
+        ]);
     }
 }
